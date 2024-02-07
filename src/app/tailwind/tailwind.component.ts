eca1d6110a732170935db74cb4942e8ed5694248
unknown <maruf.amin57@gmail.com>
2024-02-06 23:13:47 -0500

    working again

---
+++ src/app/tailwind/tailwind.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tailwind',
+  standalone: true,
+  imports: [],
+  templateUrl: './tailwind.component.html',
+  styleUrl: './tailwind.component.css'
+})
+export class TailwindComponent {
+
+}
